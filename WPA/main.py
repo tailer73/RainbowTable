545dca291f0d4b8c84c4381761f02216f952f5e9
tailer73 <zlodeynext@mail.ru>
2017-07-06 19:20:24 +0300

    добавлена функция FunctionForFindingPasswords
    теперь пользователю нужно вручную указывать ssid сети

--- WPA/main.py
+++ WPA/main.py
@@ -13,7 +13,8 @@ def Help():
     -outdir имя файла выходной директории (пустой)
 Если Вы хотите найти пароль по таблице, введите следующие параметры:
     -pcap имя файла с beacon frame и 4-мя handshakes
-    -inputdir имя директории с таблицами '''
+    -inputdir имя директории с таблицами
+    -ssid имя сети'''
     print(helpstr)
 
 def GetPasswordForLineId(inputdir, id):
@@ -30,6 +31,37 @@ def GetPasswordForLineId(inputdir, id):
         else:
             i += 1
 
+#функция для подбора пароля
+#аргументы:
+#pcapf - имя с 4-мя упорядоченными хендшейками
+#inputdir - папка с захешированными таблицами
+#ssidName - имя сети\
+#возвращает и выводит пароль в случае нахождения
+def FunctionForFindingPassword(pcapf, inputdir, ssidName):
+    sAddr, dAddr, nonces, micsFromPcap, data, isWpa1 = GetInfoFromPcap(pcapf)
+
+    try:  # попытка открыть файл с ssid
+        fileWithSSIDs = open(inputdir + '/hash_' + ssidName, 'r')
+    except FileNotFoundError:  # в случае исключения возвращает False и выводит соответствующее сообщение
+        print("Ну существует такого ssid в таблице\n")
+        return
+    A, B = MakeAB(nonces[0], nonces[1], sAddr, dAddr)  # получаем значения A и B для генерации PTK (см описание MakeAB)
+    id = 0
+    for hash in fileWithSSIDs:  # пробегаемся по каждому хешу в папке
+        if hash[-1] == '\n':
+            hash = hash[:-1]
+        pmk = a2b_hex(hash)  # записываем pmk без \n
+        mics = MakeMIC(pmk, A, B, data, isWpa1)  # считаем MIC
+        # если посчитанный mic (первый) совпадает с mic из pcap-файла - пароль найден
+        if (b2a_hex(mics[0]).decode().upper()[:-8] == b2a_hex(micsFromPcap[0]).upper().decode()):
+            passw = GetPasswordForLineId(inputdir, id)
+            print('password is: ' + passw)  # смотрим номер пароля в файле с паролями
+            return passw
+        id += 1
+    print ('Пароль не найден')
+    return  ''
+
+
 
 def main():
     #поля для дальнейшей работы
@@ -39,6 +71,7 @@ def main():
     outdir = '' #папка для хранения таблиц
     pcapf = '' #pcap-файл с хендшейками
     inputdir = '' #папка с таблицами, по которым будет произведена атака
+    ssidName = '' #имя сети
     if len(sys.argv) == 1:
         Help()#если аргуметов нет - выводим help-сообщение
         return
@@ -56,30 +89,14 @@ def main():
             pcapf = sys.argv[i + 1]
         if sys.argv[i] == '-inputdir':
             inputdir = sys.argv[i + 1]
+        if sys.argv[i] == '-ssid':
+            ssidName = sys.argv[i+1]
     if not (ssidf == '') and not (dictf == '') and not (outdir == ''): #если все 3 переменные заполнены
         #заполняем папку таблицами: для каждого ssid - все пароли из словаря
         MakeWpaHashesAndWriteInFileForFileWithSSIDsAndOneFileWithPasswords(dictf, ssidf, outdir)
 
-    if not (pcapf == '') and not (inputdir == ''): #если заполнены поля с pcap-файлом и папкой с таблицами
-        ssid, sAddr, dAddr, nonces, micsFromPcap, data, isWpa1 = GetInfoFromPcap(pcapf)
-
-        try:  # попытка открыть файл с ssid
-            fileWithSSIDs = open(inputdir+ '/hash_' + ssid, 'r')
-        except FileNotFoundError:  # в случае исключения возвращает False и выводит соответствующее сообщение
-            print("Ну существует такого ssid в таблице\n")
-            return
-        A, B = MakeAB(nonces[0], nonces[1], sAddr, dAddr)#получаем значения A и B для генерации PTK (см описание MakeAB)
-        id = 0
-        for hash in fileWithSSIDs:#пробегаемся по каждому хешу в папке
-            if hash[-1] == '\n':
-                hash = hash[:-1]
-            pmk = a2b_hex(hash)#записываем pmk без \n
-            mics = MakeMIC(pmk, A, B, data, isWpa1) #считаем MIC
-            #если посчитанный mic (первый) совпадает с mic из pcap-файла - пароль найден
-            if (b2a_hex(mics[0]).decode().upper()[:-8] == b2a_hex(micsFromPcap[0]).upper().decode()):
-                print('password is: ' + GetPasswordForLineId(inputdir, id))#смотрим номер пароля в файле с паролями
-                return
-            id += 1
+    if not (pcapf == '') and not (inputdir == '') and not (ssidName == ''): #если заполнены поля с pcap-файлом и папкой с таблицами
+        FunctionForFindingPassword(pcapf,inputdir, ssidName) #функция для поиска пароля
 
 
 
